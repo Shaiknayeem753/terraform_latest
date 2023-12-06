07d091a4ad053af45df632e8fad77c487cbb991e
nayeem <snayeem753@gmail.com>
2023-12-06 11:32:03 +0530

    1st commit

---
+++ modules/s3/main.tf
@@ -0,0 +1,15 @@
+# create s3 bucket
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name # change this
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "var.dynamodb_name"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ modules/s3/variable.tf
@@ -0,0 +1,2 @@
+variable "bucket_name" {}
+variable  "dynamodb_name" {}
