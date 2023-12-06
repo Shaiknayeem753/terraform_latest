5dc73bf35a488d2d3f506db8a93787539ceae10d
nayeem <snayeem753@gmail.com>
2023-12-06 14:14:14 +0530

    update backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,5 @@
 terraform {
-<<<<<<< HEAD
   backend "s3" {
-=======
-  backend "./modules/s3" {
->>>>>>> 6450700cae10fd4487272f8f9c865a22b9056709
     bucket         = "mybucketeks10"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
