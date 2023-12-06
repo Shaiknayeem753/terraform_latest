ccd1d60b4cbc2ef4f9f931eb93735f246449f006
nayeem <snayeem753@gmail.com>
2023-12-06 14:17:18 +0530

    update backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mybucketeks10"
+    bucket         = "mybucketeksk10"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
